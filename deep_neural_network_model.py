0f6f21815ebb6963f4ce25cda998b73523a52f72
Enes Özeren <enesozeren7@gmail.com>
2023-08-24 14:39:35 +0300

    func explanations and init func fix

--- deep_neural_network_model.py
+++ deep_neural_network_model.py
@@ -5,7 +5,7 @@ class DNN:
         pass
 
     def train(self, X, Y, layer_dims, epoch=1_000, learning_rate=0.001):
-        """Trains the neural network with given parameters and data X, Y"""
+        """Train the neural network with given parameters and data X, Y"""
 
         layer_dims.insert(0, X.shape[1])
         parameters = self.initialize_parameters(layer_dims)
@@ -17,7 +17,7 @@ class DNN:
             self.parameters = self.update_parameters(learning_rate)
 
     def predict(self, X):
-        """Predicts labels for given data X"""
+        """Predict labels for given data X"""
         pass
 
     def initialize_parameters(self, layer_dims):
@@ -25,19 +25,23 @@ class DNN:
         
         parameters = {}
         for l in range(1, len(layer_dims)):
-            parameters["W"+str(l)] = np.random.rand(layer_dims[l], layer_dims[l-1])
+            parameters["W"+str(l)] = np.random.rand(layer_dims[l], layer_dims[l-1]) * np.sqrt(2/layer_dims[l-1])
             parameters["b"+str(l)] = np.zeros((layer_dims[l], 1))
 
         return parameters
 
     def forward_prop(self):
+        """Propagate forward in network"""
         pass
 
     def backward_prop(self):
+        """Propagate backward in the network with calculating gradients"""
         pass
 
     def cost(self):
+        """Cross Entropy Loss is applied as Cost (Emprical Risk) """
         pass
 
     def update_parameters(self, learning_rate):
+        """Update parameters with gradients"""
         pass
