1146d20417117d6a50b26e19284c07308aa870d8
Enes Özeren <enesozeren7@gmail.com>
2023-08-25 14:23:01 +0300

    forward prop and cost funcs are applied and some comments

--- debugging.py
+++ debugging.py
@@ -2,4 +2,7 @@ from deep_neural_network_model import DNN
 import numpy as np
 
 model = DNN()
-model.train(X=np.array([[1,2,3],[4,5,6]]), Y=[1,0], layer_dims=[3,2,1])
+X = np.array([[1,2,3], [4,5,6], [7,8,9], [10,11,12]]).T
+Y = np.array([1,0,0,1]).reshape(1, 4)
+
+model.train(X=X, Y=Y, layer_dims=[5,4,1])
--- deep_neural_network_model.py
+++ deep_neural_network_model.py
@@ -1,47 +1,73 @@
 import numpy as np
 
 class DNN:
+    """
+    Structure: A Feedforward Neural Network
+    Optimization: Gradient Descent Algorithm
+    """
     def __init__(self) -> None:
         pass
 
     def train(self, X, Y, layer_dims, epoch=1_000, learning_rate=0.001):
         """Train the neural network with given parameters and data X, Y"""
 
-        layer_dims.insert(0, X.shape[1])
-        parameters = self.initialize_parameters(layer_dims)
+        layer_dims.insert(0, X.shape[0])
+        self.layer_dims = layer_dims
+        parameters = self.initialize_parameters()
 
         for i in range(epoch):
-            self.forward_prop()
-            self.cost()
-            self.backward_prop()
-            self.parameters = self.update_parameters(learning_rate)
+            # Forward prop and output of the last layer
+            forward_vars = self.forward_prop(parameters, X)
+            predictions = forward_vars["A"+str(len(layer_dims)-1)]
+
+            # Cost function is applied
+            cost_val = self.cost(predictions, Y)
+            
+            # Backpropagation for calculating gradients and updating parameters
+            grads = self.backward_prop(forward_vars, cost_val)
+            parameters = self.update_parameters(learning_rate, grads)
+
+        self.learned_parameters = parameters
 
     def predict(self, X):
         """Predict labels for given data X"""
         pass
 
-    def initialize_parameters(self, layer_dims):
+    def initialize_parameters(self):
         """Initialize parameters with He initialization method"""
         
         parameters = {}
-        for l in range(1, len(layer_dims)):
-            parameters["W"+str(l)] = np.random.rand(layer_dims[l], layer_dims[l-1]) * np.sqrt(2/layer_dims[l-1])
-            parameters["b"+str(l)] = np.zeros((layer_dims[l], 1))
+        for l in range(1, len(self.layer_dims)):
+            parameters["W"+str(l)] = np.random.rand(self.layer_dims[l], self.layer_dims[l-1]) * np.sqrt(2/self.layer_dims[l-1])
+            parameters["b"+str(l)] = np.zeros((self.layer_dims[l], 1))
 
         return parameters
 
-    def forward_prop(self):
+    def forward_prop(self, parameters, X):
         """Propagate forward in network"""
-        pass
+        
+        forward_vars = {"A0": X}
+
+        for l in range(1, len(self.layer_dims)):
+            forward_vars["Z"+str(l)] = np.dot(parameters["W"+str(l)], forward_vars["A"+str(l-1)]) + parameters["b"+str(l)]
+            forward_vars["A"+str(l)] = self.sigmoid(forward_vars["Z"+str(l)])
+
+        return forward_vars
 
     def backward_prop(self):
         """Propagate backward in the network with calculating gradients"""
         pass
 
-    def cost(self):
+    def cost(self, Y_hat, Y):
         """Cross Entropy Loss is applied as Cost (Emprical Risk) """
-        pass
+        m = Y.shape[1]
+        cost_value = (1/m) * np.sum(-(Y*np.log(Y_hat) + (1-Y)*np.log(1-Y_hat)))
 
-    def update_parameters(self, learning_rate):
+        return cost_value
+
+    def update_parameters(self, learning_rate, grads):
         """Update parameters with gradients"""
         pass
+
+    def sigmoid(self, Z):
+        return 1 / (1 + np.exp(-Z))
